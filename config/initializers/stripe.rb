844291e0743afb342215b74b18a667c197e6ee14
Daniel Cardenas <dan85.cardenas@gmail.com>
2015-08-30 08:38:47 -0400

    Delete stripe.rb

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_live_CBRK64PqRV5qNmPQOMB2Yrwh'],
-  :secret_key      => ENV['sk_live_REszAW0b84G5VXcLC4TCudMn']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
